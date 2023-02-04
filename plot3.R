51c3839c5a82466acbf4cbb786d8b8947e248a4e
saljoey361 <saljoey361@gmail.com>
2023-02-03 23:35:20 -0800

    v1

---
+++ plot3.R
@@ -0,0 +1,11 @@
+data<-read.table('household_power_consumption.txt',sep=";",na.strings="?", header = T)
+data<-data[data$Date %in% c("1/2/2007","2/2/2007"), ]
+date<-as.Date(data[, 1],format = "%d/%m/%Y")
+time<-strptime(data[, 2], format = "%H:%M:%S")
+timex<-as.POSIXct(paste(date, data[, 2]))
+png(filename="plot3.png", width=480, height=480)
+plot(timex,data[, 7],xlab="",type="l",ylab="Energy sub metering")
+lines(timex,data[, 8],col="red")
+lines(timex,data[, 9],col="blue")
+legend("topright",lty = 1,c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),col=c("black","red","blue"))
+dev.off()
